f766db4da17aa20cc39a18214319228886192691
emanuelecoppo <emanuelecoppo@gmail.com>
2019-01-21 11:47:54 +0100

    a

--- functions/finale.js
+++ functions/finale.js
@@ -7,10 +7,16 @@ var finaleState = {
         game.world.setBounds(0, 0, 1024, 768);
 
         game.time.events.add(1000, function(){
-            game.add.sprite(0, 0, 'finale');
+            finale = game.add.sprite(0, 0, 'finale');
+            fine = game.add.text(game.world.centerX, game.world.centerY + 50, 'Fine', {font: "50px DIN", fill:'#fff'})
+            fine.anchor.setTo(.5); fine.alpha = 0;
             game.camera.flash(0xffffff, 1000);
+            game.camera.onFlashComplete.add(function(){game.stage.backgroundColor = "#000"});
             music = game.add.audio('finaleMusic', .5).play();
+            game.add.tween(finale).to( {alpha: 0}, 1000).delay(7000).start();
+            game.add.tween(fine).to( {alpha: 1}, 1000).delay(8000).start();
         }, this);
+
         game.time.events.add(12000, nextState, this)
 
         function nextState() {
